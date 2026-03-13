52dea626ded07d228dbb10de981cde43e40f7831
high6120-eng <high6120@gmail.com>
2026-03-13 23:13:16 +0900

    수정 내용에 대한 간단한 메시지

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // 빌드 시 타입 에러가 있어도 무시하고 배포 진행
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // 빌드 시 린트(문법 검사) 에러가 있어도 무시
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* 여기에 설정 옵션을 넣습니다 */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
